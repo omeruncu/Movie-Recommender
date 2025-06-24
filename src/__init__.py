41037ebe35d5eaa07a3d58b09ba83378226b7822
omeru <omeruncu@outlook.com>
2025-06-24 11:37:00 +0300

    Fix imports

--- src/__init__.py
+++ src/__init__.py
@@ -6,6 +6,7 @@ Altındaki recommender modüllerini doğrudan import etmeye izin verir.
 from .user_based_recommender import user_based_recommender
 from .item_based_recommender import item_based_recommender
 from .hybrid_recommender import hybrid_recommender
+from .data_loader import get_random_user_id, load_and_prepare_data
 
 __all__ = [
     "user_based_recommender",
